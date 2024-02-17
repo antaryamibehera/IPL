59f541e7177ec9e65849caca63281befe57c18c7
antaryami <papu@gmail.com>
2024-02-17 13:46:40 +0530

    Branch change in 2nd

--- Addition/src/com/add/Addition.java
+++ Addition/src/com/add/Addition.java
@@ -3,12 +3,8 @@ package com.add;
 public class Addition {
 
 	public static void main(String[] args) {
-		int a=10,b=20,c;
-		c=a+b;
-		System.out.println(c);
-		
-		int d=60,e=5,f;
-		f=d-e;
-		System.out.println(f);
+		for(int u=1;u<=5;u++) {
+			System.out.println(u);
+		}
 	}
 }
